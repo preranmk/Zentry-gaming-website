16e4997eb01720d9af4cada29b14a8c90ed1782c
preranmk <preranmk@gmail.com>
2025-03-11 15:37:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
 import Features from "./components/Features";
-import Story from "./components/Story";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
